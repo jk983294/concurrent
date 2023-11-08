23c0a497eae41502883d1c5fbe1308aff754657a
jk983294 <jk983294@gmail.com>
2023-11-08 17:25:06 +0800

    write column storage

--- zergtool/example/write_feather.cpp
+++ zergtool/example/write_feather.cpp
@@ -9,17 +9,42 @@ int main() {
     std::vector<double> d_vec(nrOfRows);
     std::vector<float> f_vec(nrOfRows);
     std::vector<int> i_vec(nrOfRows);
+    std::vector<std::string> s_vec(nrOfRows);
+    std::vector<bool> b_vec(nrOfRows);
     for (int i = 0; i < nrOfRows; ++i) {
         d_vec[i] = i;
         f_vec[i] = i;
         i_vec[i] = i;
+        s_vec[i] = std::to_string(i);
+        b_vec[i] = i % 2 == 0;
     }
 
     std::vector<OutputColumnOption> options;
     options.push_back({1, d_vec.data(), "my_double"});
     options.push_back({2, f_vec.data(), "my_float"});
     options.push_back({3, i_vec.data(), "my_int"});
+    options.push_back({4, &s_vec, "my_str"});
+    options.push_back({5, &b_vec, "my_bool"});
 
     write_feather(path_, nrOfRows, options);
+
+    FeatherReader reader;
+    reader.read(path_);
+    printf("rows=%zu\n", reader.rows);
+    for (auto& col : reader.cols) {
+        if (col.type == 1) {
+            auto& vec = *reinterpret_cast<std::vector<double>*>(col.data);
+            printf("col %s type=%d data=%f\n", col.name.c_str(), col.type, vec.front());
+        } else if (col.type == 3) {
+            auto& vec = *reinterpret_cast<std::vector<int>*>(col.data);
+            printf("col %s type=%d data=%d\n", col.name.c_str(), col.type, vec.front());
+        } else if (col.type == 4) {
+            auto& vec = *reinterpret_cast<std::vector<std::string>*>(col.data);
+            printf("col %s type=%d data=%s\n", col.name.c_str(), col.type, vec.front().c_str());
+        } else if (col.type == 5) {
+            auto& vec = *reinterpret_cast<std::vector<bool>*>(col.data);
+            printf("col %s type=%d data=%d\n", col.name.c_str(), col.type, (int)vec.front());
+        }
+    }
     return 0;
 }
--- zergtool/include/zerg_feather.h
+++ zergtool/include/zerg_feather.h
@@ -8,7 +8,7 @@
 #include <zerg_file.h>
 
 namespace ztool {
-bool write_feather(std::string path_, size_t nrow, const std::vector<OutputColumnOption>& cols) {
+inline bool write_feather(std::string path_, size_t nrow, const std::vector<OutputColumnOption>& cols) {
     path_ = FileExpandUser(path_);
     std::vector<std::shared_ptr<arrow::Array>> arrays;
     arrow::SchemaBuilder sb_;
@@ -35,6 +35,22 @@ bool write_feather(std::string path_, size_t nrow, const std::vector<OutputColum
             std::ignore = tmp_builder->Finish(&tmp_a);
             arrays.push_back(tmp_a);
             std::ignore = sb_.AddField(arrow::field(option.name, arrow::int32()));
+        } else if (option.type == 4) {
+            auto tmp_builder = std::make_shared<arrow::StringBuilder>();
+            std::vector<std::string>& sVec = *reinterpret_cast<std::vector<std::string>*>(option.data);
+            std::ignore = tmp_builder->AppendValues(sVec);
+            std::shared_ptr<arrow::Array> tmp_a;
+            std::ignore = tmp_builder->Finish(&tmp_a);
+            arrays.push_back(tmp_a);
+            std::ignore = sb_.AddField(arrow::field(option.name, arrow::utf8()));
+        } else if (option.type == 5) {
+            auto tmp_builder = std::make_shared<arrow::BooleanBuilder>();
+            const std::vector<bool>& bVec = *reinterpret_cast<const std::vector<bool>*>(option.data);
+            std::ignore = tmp_builder->AppendValues(bVec);
+            std::shared_ptr<arrow::Array> tmp_a;
+            std::ignore = tmp_builder->Finish(&tmp_a);
+            arrays.push_back(tmp_a);
+            std::ignore = sb_.AddField(arrow::field(option.name, arrow::boolean()));
         } else {
             throw std::runtime_error("write_feather un support type");
         }
@@ -60,4 +76,130 @@ bool write_feather(std::string path_, size_t nrow, const std::vector<OutputColum
         return true;
     }
 }
+
+namespace {
+inline void get_data_by_arrow_type_helper(double val, int& out) { out = static_cast<int>(std::lround(val)); }
+inline void get_data_by_arrow_type_helper(int val, int& out) { out = val; }
+inline void get_data_by_arrow_type_helper(int64_t val, int& out) { out = val; }
+inline void get_data_by_arrow_type_helper(double val, double& out) { out = val; }
+inline void get_data_by_arrow_type_helper(float val, double& out) { out = val; }
+
+template <typename TIn, typename TOut>
+inline void get_array_data(std::vector<TOut>& vec, std::shared_ptr<arrow::Table>& Tbl, int idx, int64_t rows) {
+    vec.resize(rows);
+    auto pChArray = Tbl->column(idx);
+    int NChunks = pChArray->num_chunks();
+    int i = 0;
+    for (int n = 0; n < NChunks; n++) {
+        auto pArray = pChArray->chunk(n);
+        int64_t ArrayRows = pArray->length();
+        auto pTypedArray = std::dynamic_pointer_cast<TIn>(pArray);
+        const auto* pData = pTypedArray->raw_values();
+        for (int64_t j = 0; j < ArrayRows; j++) {
+            TOut tmp;
+            get_data_by_arrow_type_helper(pData[j], tmp);
+            vec[i++] = tmp;
+        }
+    }
+}
+
+inline void get_array_data_string(std::vector<std::string>& vec, std::shared_ptr<arrow::Table>& Tbl, int idx,
+                                  int64_t rows) {
+    vec.resize(rows);
+    auto pChArray = Tbl->column(idx);
+    int NChunks = pChArray->num_chunks();
+    int i = 0;
+    for (int n = 0; n < NChunks; n++) {
+        std::shared_ptr<arrow::Array> pArray = pChArray->chunk(n);
+        int64_t ArrayRows = pArray->length();
+        auto pTypedArray = std::dynamic_pointer_cast<arrow::StringArray>(pArray);
+        for (int64_t j = 0; j < ArrayRows; j++) {
+            std::string tmp = pTypedArray->GetString(j);
+            vec[i++] = tmp;
+        }
+    }
+}
+
+inline void get_array_data_bool(std::vector<bool>& vec, std::shared_ptr<arrow::Table>& Tbl, int idx, int64_t rows) {
+    vec.resize(rows);
+    auto pChArray = Tbl->column(idx);
+    int NChunks = pChArray->num_chunks();
+    int i = 0;
+    for (int n = 0; n < NChunks; n++) {
+        std::shared_ptr<arrow::Array> pArray = pChArray->chunk(n);
+        int64_t ArrayRows = pArray->length();
+        auto pTypedArray = std::dynamic_pointer_cast<arrow::BooleanArray>(pArray);
+        for (int64_t j = 0; j < ArrayRows; j++) {
+            vec[i++] = pTypedArray->Value(j);
+        }
+    }
+}
+}  // namespace
+
+struct FeatherReader {
+    FeatherReader() = default;
+    ~FeatherReader() {
+        for (auto* vec : ints) delete vec;
+        for (auto* vec : doubles) delete vec;
+        for (auto* vec : bools) delete vec;
+        for (auto* vec : strs) delete vec;
+    }
+    void read(std::string path_) {
+        path_ = FileExpandUser(path_);
+        auto format = std::make_shared<arrow::dataset::IpcFileFormat>();
+        std::shared_ptr<arrow::fs::FileSystem> fs = std::make_shared<arrow::fs::LocalFileSystem>();
+        std::string uri = "file://" + path_;
+        auto factory =
+            arrow::dataset::FileSystemDatasetFactory::Make(uri, format, arrow::dataset::FileSystemFactoryOptions())
+                .ValueOrDie();
+        auto dataset = factory->Finish().ValueOrDie();
+        auto scan_builder = dataset->NewScan().ValueOrDie();
+        auto scanner = scan_builder->Finish().ValueOrDie();
+        std::shared_ptr<arrow::Table> table1 = scanner->ToTable().ValueOrDie();
+        auto schema_ = table1->schema();
+        auto col_cnt = table1->num_columns();
+        rows = table1->num_rows();
+
+        for (int i = 0; i < col_cnt; ++i) {
+            auto f_ = schema_->field(i);
+            auto type_ = f_->type()->name();
+
+            if (type_ == "int32") {
+                auto* pVec = new std::vector<int>();
+                get_array_data<arrow::Int32Array, int>(*pVec, table1, i, rows);
+                ints.push_back(pVec);
+                cols.push_back({3, pVec, f_->name()});
+            } else if (type_ == "double") {
+                auto* pVec = new std::vector<double>();
+                get_array_data<arrow::DoubleArray, double>(*pVec, table1, i, rows);
+                doubles.push_back(pVec);
+                cols.push_back({1, pVec, f_->name()});
+            } else if (type_ == "float") {
+                auto* pVec = new std::vector<double>();
+                get_array_data<arrow::FloatArray, double>(*pVec, table1, i, rows);
+                doubles.push_back(pVec);
+                cols.push_back({1, pVec, f_->name()});
+            } else if (type_ == "utf8") {
+                auto* pVec = new std::vector<std::string>();
+                get_array_data_string(*pVec, table1, i, rows);
+                strs.push_back(pVec);
+                cols.push_back({4, pVec, f_->name()});
+            } else if (type_ == "bool") {
+                auto* pVec = new std::vector<bool>();
+                get_array_data_bool(*pVec, table1, i, rows);
+                bools.push_back(pVec);
+                cols.push_back({5, pVec, f_->name()});
+            } else {
+                printf("unknown %s,%s\n", f_->name().c_str(), type_.c_str());
+            }
+        }
+    }
+
+    std::vector<std::vector<int>*> ints;
+    std::vector<std::vector<double>*> doubles;
+    std::vector<std::vector<bool>*> bools;
+    std::vector<std::vector<std::string>*> strs;
+    std::vector<OutputColumnOption> cols;
+    uint64_t rows{0};
+};
 }  // namespace ztool
